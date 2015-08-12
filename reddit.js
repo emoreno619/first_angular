ec04907465a69ead34bc5a1cf9c1c81d18acbad5
Eduardo Moreno <emoreno@uchicago.edu>
2015-08-12 15:12:24 -0700

    fixed comment issue

--- reddit.js
+++ reddit.js
@@ -9,30 +9,51 @@ app.controller("redditController", function($scope){
    	if($scope.post.title && $scope.post.author && $scope.post.description && $scope.post.image){
 	   	var newPost = $scope.post
 	   	$scope.post.votes = 0
+	   	$scope.post.showComments = false
+	   	$scope.post.showCommentForm = false
 	   	$scope.post.comments = []
+
+	    this.toggleCommentForm = function(){
+	   		$scope.comment = {}
+	   		this.showCommentForm = !this.showCommentForm
+	   	}
+
+	   this.addComment = function(){
+
+	   		if($scope.comment.author && $scope.comment.text){
+	   			var newComment = this.comment
+	   			console.log($scope.post.title)
+	   			this.post.comments.push(newComment)
+	   			this.showCommentForm = !this.showCommentForm
+	   		}
+	   	}
+
+	   	this.toggleComments = function(){
+	   		this.showComments = !this.showComments
+	   	}
 	   	$scope.posts.push(newPost)
 	   	$scope.showForm = !$scope.showForm
    	}
    }
 
-   $scope.addComment = function(){
+   // $scope.addComment = function(){
 
-   	if($scope.comment.author && $scope.comment.text){
-   		var newComment = $scope.comment
-   		console.log($scope.post.title)
-   		$scope.post.comments.push(newComment)
-   		$scope.showCommentForm = !$scope.showCommentForm
-   	}
-   }
+   // 	if($scope.comment.author && $scope.comment.text){
+   // 		var newComment = $scope.comment
+   // 		console.log($scope.post.title)
+   // 		$scope.post.comments.push(newComment)
+   // 		$scope.showCommentForm = !$scope.showCommentForm
+   // 	}
+   // }
 
-   $scope.toggleComments = function(){
-   	$scope.showComments = !$scope.showComments
-   }
+   // $scope.toggleComments = function(){
+   // 	$scope.showComments = !$scope.showComments
+   // }
 
-   $scope.toggleCommentForm = function(){
-   	$scope.comment = {}
-   	$scope.showCommentForm = !$scope.showCommentForm
-   }
+   // $scope.toggleCommentForm = function(){
+   // 	$scope.comment = {}
+   // 	$scope.showCommentForm = !$scope.showCommentForm
+   // }
 
    $scope.toggleForm = function(){
    	$scope.post = {}
